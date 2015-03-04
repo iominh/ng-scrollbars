d6786792bd69b08eeed2bf2073e75f3be2bfb8a3
minh nguyen <minh.9.nguyen@gmail.com>
2015-03-03 19:31:34 -0500

    test ng-annotate

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,11 @@
 var gulp = require('gulp');
 var uglify = require('gulp-uglifyjs');
+var ngAnnotate = require('gulp-ng-annotate');
 
 gulp.task('compress', function() {
 	gulp.src('src/*.js')
-			.pipe(uglify('scrollbars.min.js'))
+    .pipe(ngAnnotate())
+    .pipe(uglify('scrollbars.min.js'))
 			.pipe(gulp.dest('dist'))
 });
 
